981a3e399a5284d0e624906e25363f3153ca7222
giorgi225 <82880106+giorgi225@users.noreply.github.com>
2023-02-03 13:41:08 +0400

    loader

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="w-full max-w-[1300px] mx-auto px-6 mt-12">
-        <div class="flex flex-col gap-32 mb-32">
-            <a href="https://cinemania22.netlify.app/" target="_blank" class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
+        <div @load="load" class="flex flex-col gap-32 mb-32">
+            <a href="https://cinemania22.netlify.app/" target="_blank"
+                class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
                 <div class="flex flex-col gap-12">
                     <div class="flex flex-col">
                         <h4 class="text-2xl">Online Streaming Movie Website Cinemania (DEMO)</h4>
@@ -12,14 +13,18 @@
                     </button>
                 </div>
 
-                <div class="w-full bg-gray rounded px-16 pt-12">
+                <div class="relative w-full bg-gray rounded px-16 pt-12">
                     <div class="w-full">
+                        <div v-if="!loader" class="absolute w-full bg-gray h-full flex items-center justify-center left-0 top-0">
+                            <div class="spiner w-12 h-12 border-2 border-fontColor border-r-transparent rounded-full"></div>
+                        </div>
                         <img src="../assets/images/works/cinemania.png" alt="">
                     </div>
                 </div>
             </a>
 
-            <a href="https://giorgi-tm-todo.netlify.app/" target="_blank" class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
+            <a href="https://giorgi-tm-todo.netlify.app/" target="_blank"
+                class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
                 <div class="flex flex-col gap-12">
                     <div class="flex flex-col">
                         <h4 class="text-2xl">One page ToDo Application (DEMO)</h4>
@@ -30,14 +35,18 @@
                     </button>
                 </div>
 
-                <div class="w-full bg-gray rounded px-16 pt-12">
+                <div class="relative w-full bg-gray rounded px-16 pt-12">
                     <div class="w-full">
+                        <div v-if="!loader" class="absolute w-full bg-gray h-full flex items-center justify-center left-0 top-0">
+                            <div class="spiner w-12 h-12 border-2 border-fontColor border-r-transparent rounded-full"></div>
+                        </div>
                         <img src="../assets/images/works/todo.png" alt="">
                     </div>
                 </div>
             </a>
 
-            <a href="https://giorgi-tm-todo.netlify.app/" target="_blank" class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
+            <a href="https://giorgi-tm-todo.netlify.app/" target="_blank"
+                class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
                 <div class="flex flex-col gap-12">
                     <div class="flex flex-col">
                         <h4 class="text-2xl">One page Quiz Application (DEMO)</h4>
@@ -48,14 +57,18 @@
                     </button>
                 </div>
 
-                <div class="w-full bg-gray rounded px-16 pt-12">
+                <div class="relative w-full bg-gray rounded px-16 pt-12">
                     <div class="w-full">
+                        <div v-if="!loader" class="absolute w-full bg-gray h-full flex items-center justify-center left-0 top-0">
+                            <div class="spiner w-12 h-12 border-2 border-fontColor border-r-transparent rounded-full"></div>
+                        </div>
                         <img src="../assets/images/works/quiz.png" alt="">
                     </div>
                 </div>
             </a>
 
-            <a href="https://trendy22.netlify.app/#/" target="_blank" class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
+            <a href="https://trendy22.netlify.app/#/" target="_blank"
+                class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
                 <div class="flex flex-col gap-12">
                     <div class="flex flex-col">
                         <h4 class="text-2xl">Trendy - Personal Portfolio Website Template (DEMO)</h4>
@@ -66,15 +79,19 @@
                     </button>
                 </div>
 
-                <div class="w-full bg-gray rounded px-16 pt-12">
+                <div class="relative w-full bg-gray rounded px-16 pt-12">
                     <div class="w-full">
+                        <div v-if="!loader" class="absolute w-full bg-gray h-full flex items-center justify-center left-0 top-0">
+                            <div class="spiner w-12 h-12 border-2 border-fontColor border-r-transparent rounded-full"></div>
+                        </div>
                         <img src="../assets/images/works/trendy.png" alt="">
                     </div>
                 </div>
             </a>
 
 
-            <a href="https://porto-wwx2.vercel.app/#/" target="_blank" class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
+            <a href="https://porto-wwx2.vercel.app/#/" target="_blank"
+                class="hover:scale-x-[0.99] active:scale-x-[1] transition-all w-fill grid grid-cols-[40%,40%] items-start justify-between">
                 <div class="flex flex-col gap-12">
                     <div class="flex flex-col">
                         <h4 class="text-2xl">Portos - Personal Portfolio Website Template (DEMO)</h4>
@@ -85,8 +102,11 @@
                     </button>
                 </div>
 
-                <div class="w-full bg-gray rounded px-16 pt-12">
+                <div class="relative w-full bg-gray rounded px-16 pt-12">
                     <div class="w-full">
+                        <div v-if="!loader" class="absolute w-full bg-gray h-full flex items-center justify-center left-0 top-0">
+                            <div class="spiner w-12 h-12 border-2 border-fontColor border-r-transparent rounded-full"></div>
+                        </div>
                         <img src="../assets/images/works/porto.png" alt="">
                     </div>
                 </div>
@@ -98,6 +118,16 @@
 
 <script>
     export default {
-        name: "Works"
+        name: "Works",
+        data() {
+            return {
+                loader : false,
+            }
+        },
+        methods: {
+            load() {
+                this.loader = false
+            }
+        }
     }
 </script>
