5c73def263f3fb493b802514a2db97e1ccd31740
giorgi225 <gigi.shalamberidze2022@gmail.com>
2025-04-06 00:35:44 +0400

    video fix

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/hero.mp4",
-                headers: [
-                    {
-                        key: "Accept-Ranges",
-                        value: "bytes",
-                    },
-                ],
-            },
-        ];
-    },
-}
-    ;
 
+}
 export default nextConfig;
